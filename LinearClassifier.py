dbe1b7a959474de1495ff0b28e7294d8f64bffbf
Merrill <mxd400@icloud.com>
2024-06-03 17:47:10 +0100

    final changes to linear classifier maker

--- LinearClassifier.py
+++ LinearClassifier.py
@@ -38,17 +38,14 @@ x is point on the hyperplane
 class Hyperplane:
     def __init__(self, theta, theta_zero, x_hyperplane=None):
         self.theta = np.array(theta)
-        self.hyperplaneform = np.array([self.theta[1], self.theta[0] * -1])
+        #self.hyperplaneform = np.array([self.theta[1], self.theta[0] * -1])
         if theta_zero is not None:
             self.theta_zero = theta_zero
         else:
             if x_hyperplane is None:
                 raise Exception("Both theta_zero and a point on hyperplane cant be none")
-            self.theta_zero = self.calcCfromgradientandPoint(self.hyperplaneform,x_hyperplane)
+            self.theta_zero = -1 * ((self.theta[0] * x_hyperplane[0]) + (self.theta[1] * x_hyperplane[1]))
 
-
-    def calcCfromgradientandPoint(self,grad,x):
-        return x[1] - (grad[1] / grad[0] * x[0] )
     def getX2onHyperplane(self,X1):
         return -1 * ((self.theta[0] * X1) + self.theta_zero) / self.theta[1]
 
@@ -59,39 +56,66 @@ class Hyperplane:
     def drawHyperplane(self,point=None):
 
         thetasumsquare = pow(self.theta[0], 2) + pow(self.theta[1], 2)
-        startx = min(0,-1 * self.theta_zero * self.theta[0] / thetasumsquare)
-        endx = max(self.theta[0],- 1 * self.theta_zero * self.theta[0] / thetasumsquare)
+        #
+        #
+        #startx = min(0,-1 * self.theta_zero * self.theta[0] / thetasumsquare)
+        #endx = max(self.theta[0],- 1 * self.theta_zero * self.theta[0] / thetasumsquare)
 
-        normal = [np.linspace(startx, endx),
+        # normal = [np.linspace(startx, endx),
+        #           [i * self.theta[1] / self.theta[0]
+        #            for i in np.linspace(startx, endx)]]
+
+        intersectx = -1 * self.theta_zero * self.theta[0] / thetasumsquare
+        intersecty = -1 * self.theta_zero * self.theta[1] / thetasumsquare
+
+        normal = [np.linspace(0, self.theta[0]),
                   [i * self.theta[1] / self.theta[0]
-                   for i in np.linspace(startx, endx)]]
+                   for i in np.linspace(0, self.theta[0])]]
 
+        print(f"The normal and the hyperplane intersect at X: {intersectx} Y: {intersecty}")
         #print(f"minimum X: {startx}, endx = {endx}")
-
-        hyperplane = [np.linspace(-10, 10), [self.getX2onHyperplane(i) for i in np.linspace(-10, 10)]]
 
         plt.plot(normal[0], normal[1], color='blue', linestyle='--',label="normal")
         plt.axhline(0, linestyle='solid', color='black')
         plt.axvline(0, linestyle='solid', color='black')
         plt.axis('equal')
-        plt.plot(hyperplane[0], hyperplane[1], color='red', linestyle='solid',label="hyperplane")
+        start_hypx = -10
+        end_hypx = 10
         if point is not None:
             plt.axhline(point[1], linestyle=':', color='green')
             plt.axvline(point[0], linestyle=':', color='green')
+            if abs(point[0]) > 10:
+                if point[0] >= 0:
+                    end_hypx = point[0] + 2
+                else:
+                    start_hypx = point[0] - 2
+
+        hyperplane = [np.linspace(start_hypx, end_hypx),
+                      [self.getX2onHyperplane(i) for i in np.linspace(start_hypx, end_hypx)]]
+
+        plt.plot(hyperplane[0], hyperplane[1], color='red', linestyle='solid',label="hyperplane")
+
         plt.legend(loc="upper left")
         plt.show()
 
 
 test = Hyperplane([1,2],1.5)
-print(test.hyperplaneform)
 #test.drawHyperplane()
 
-test2 = Hyperplane([1,2],theta_zero=None,x_hyperplane=[0,1.5])
+test2 = Hyperplane([1,2],theta_zero=None,x_hyperplane=[-0.3,-0.6])
 
 print(test.getX2onHyperplane(0))
-test.drawHyperplane()
+#test.drawHyperplane()
 test.drawHyperplane([-0.3,-0.6])
 
 
 print(test.getX1onHyperplane(-0.75))
 print(test.getX2onHyperplane(0))
+
+print(test2.theta_zero)
+
+test3 = Hyperplane([1,-2],20)
+test3.drawHyperplane()
+
+test4 = Hyperplane([-1,-2],20)
+test4.drawHyperplane([100,20])
